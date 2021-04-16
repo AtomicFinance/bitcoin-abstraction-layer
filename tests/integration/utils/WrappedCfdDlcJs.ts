551e51db7094873affa356d9fe584b4143a4fd05
Matthew Black <mblackmblackmblack@gmail.com>
2021-04-16 01:36:00 -0400

    Fix Cfd Wrapper for tests

--- tests/integration/utils/WrappedCfdDlcJs.ts
+++ tests/integration/utils/WrappedCfdDlcJs.ts
@@ -22,6 +22,9 @@ export function getWrappedCfdDlcJs(): CfdDlcJs {
       get: function (_, method) {
         if (METHODS_TO_PARALLELIZE.indexOf(method as string) !== -1) {
           return async (...args) => {
+            await new Promise((res) => {
+              res(1);
+            });
             const res = await pool.exec({ method, args });
             return res;
           };
