cdaffddc8151b19a59402f6f31c419f2a420e51e
Steven <steven@zhao.io>
2021-08-18 13:55:23 -0400

    Remove it.only

--- tests/integration/dlc/dlc.test.ts
+++ tests/integration/dlc/dlc.test.ts
@@ -489,7 +489,7 @@ describe('dlc provider', () => {
         expect(cetTx._raw.vin.length).to.equal(1);
       });
 
-      it.only('execute event 3', async () => {
+      it('execute event 3', async () => {
         const outcome = 0;
         const oracleAttestation = generateOracleAttestation(
           outcome,
