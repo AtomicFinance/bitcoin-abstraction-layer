1eacc161260f99cd898ba8265611441b2d663a37
Matthew Black <mblackmblackmblack@gmail.com>
2024-03-16 08:06:22 -0400

    --wip-- [skip ci]

--- packages/crypto/lib/index.ts
+++ packages/crypto/lib/index.ts
@@ -2,20 +2,20 @@ import bech32 from 'bech32';
 import base58 from 'bs58';
 import cryptoHash from 'crypto-hashing';
 
-function isHex(hex: string) {
+const isHex = (hex: string): boolean => {
   if (!hex.match(/([0-9]|[a-f])/gim)) return false;
 
   const buf = Buffer.from(hex, 'hex').toString('hex');
 
   return buf === hex.toLowerCase();
-}
+};
 
 /**
  * Ensure message is in buffer format.
  * @param {string} message - any string.
  * @return {string} Returns Buffer.
  */
-function ensureBuffer(message: string | Buffer | any) {
+const ensureBuffer = (message: string | Buffer | any): Buffer | boolean => {
   if (Buffer.isBuffer(message)) return message;
 
   switch (typeof message) {
@@ -30,7 +30,7 @@ function ensureBuffer(message: string | Buffer | any) {
   }
 
   return Buffer.isBuffer(message) ? message : false;
-}
+};
 
 /**
  * Get hash of a message in hex.
@@ -38,36 +38,36 @@ function ensureBuffer(message: string | Buffer | any) {
  * @param {!string|Buffer} message - Message to be hashed.
  * @return {string} Returns the hash of a string.
  */
-function hashToHex(algorithm: string, message: string | Buffer) {
+const hashToHex = (algorithm: string, message: string | Buffer): string => {
   return cryptoHash(algorithm, ensureBuffer(message)).toString('hex');
-}
+};
 
 /**
  * Get hash160 of message.
  * @param {!string|Buffer} message - message in string or Buffer.
  * @return {string} Returns the hash160 of a string.
  */
-function hash160(message: Buffer) {
+const hash160 = (message: Buffer): string => {
   return hashToHex('hash160', message);
-}
+};
 
 /**
  * Get sha256 of message.
  * @param {!string|Buffer} message - message in string or Buffer.
  * @return {string} Returns the sha256 of a string.
  */
-function sha256(message: string | Buffer) {
+const sha256 = (message: string | Buffer): string => {
   return hashToHex('sha256', message);
-}
+};
 
 /**
  * Get ripemd160 of message.
  * @param {!string|Buffer} message - message in string or Buffer.
  * @return {string} Returns the ripemd160 of a string.
  */
-function ripemd160(message: string | Buffer) {
+const ripemd160 = (message: string | Buffer): string => {
   return hashToHex('ripemd160', message);
-}
+};
 
 /**
  * Pad a hex string with '0'
@@ -76,12 +76,12 @@ function ripemd160(message: string | Buffer) {
  * @return {string} Returns a padded string with length greater or equal to the given length
  *  rounded up to the nearest even number.
  */
-function padHexStart(hex: string, lengthBytes?: number) {
+const padHexStart = (hex: string, lengthBytes?: number): string => {
   let lengthString = lengthBytes * 2 || hex.length;
   lengthString += lengthString % 2;
 
   return hex.padStart(lengthString, '0');
-}
+};
 
 export {
   /**
